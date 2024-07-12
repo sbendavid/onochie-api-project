c63cedf7fc5f65710c5020dc0a779abc5245bd49
sbendavid <samuelbendavid01@gmail.com>
2024-07-12 03:59:18 +0100

    Updated the build sh

--- build_files.sh
+++ build_files.sh
@@ -1,23 +1,38 @@
-echo "BUILD START"
+# echo "BUILD START"
 
-# create a virtual environment named 'venv' if it doesn't already exist
-python3.12 -m venv venv
+# # create a virtual environment named 'venv' if it doesn't already exist
+# python3.12 -m venv venv
 
-# activate the virtual environment
-source venv/bin/activate
+# # activate the virtual environment
+# source venv/bin/activate
 
-# build_files.sh
-python3.12 -m pip install -r requirements.txt
+# # build_files.sh
+# python3.12 -m pip install -r requirements.txt
 
-# make migrations
-python3.12 manage.py migrate 
-python3.12 manage.py collectstatic
+# # make migrations
+
+# python3.12 manage.py collectstatic
 
 
-# collect static files using the Python interpreter from venv
-python manage.py collectstatic --noinput
+# # collect static files using the Python interpreter from venv
+# python manage.py collectstatic --noinput
 
-echo "BUILD END"
+# echo "BUILD END"
 
-# [optional] Start the application here 
-# python manage.py runserver
+# # [optional] Start the application here 
+# # python manage.py runserver
+
+#!/bin/bash
+
+# Install pip if not already installed
+if ! command -v pip &> /dev/null
+then
+    echo "pip not found, installing pip"
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python get-pip.py --user
+fi
+
+# Install dependencies
+pip install -r requirements.txt
+
+python3.9 manage.py migrate 
