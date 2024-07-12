c2a279c0fe31d66324fc486bd32a588e7e913cb2
sbendavid <samuelbendavid01@gmail.com>
2024-07-12 04:08:48 +0100

    Updated the build sh

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# Ensure that Python and pip are installed
-if ! command -v python3 &> /dev/null
-then
-    echo "Python3 could not be found. Installing Python3..."
-    apt-get update
-    apt-get install -y python3 python3-pip
-fi
+# Install Python and pip
+apt-get update
+apt-get install -y python3 python3-pip python3-venv
 
 # Create and activate a virtual environment
 python3 -m venv venv
