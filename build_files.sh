db0fa604c1d6965868311c1c344b155e3ec197cc
sbendavid <samuelbendavid01@gmail.com>
2024-07-12 04:05:01 +0100

    Updated the build sh

--- build_files.sh
+++ build_files.sh
@@ -1,38 +1,25 @@
-# echo "BUILD START"
-
-# # create a virtual environment named 'venv' if it doesn't already exist
-# python3.12 -m venv venv
-
-# # activate the virtual environment
-# source venv/bin/activate
-
-# # build_files.sh
-# python3.12 -m pip install -r requirements.txt
-
-# # make migrations
-
-# python3.12 manage.py collectstatic
-
-
-# # collect static files using the Python interpreter from venv
-# python manage.py collectstatic --noinput
-
-# echo "BUILD END"
-
-# # [optional] Start the application here 
-# # python manage.py runserver
-
 #!/bin/bash
 
-# Install pip if not already installed
-if ! command -v pip &> /dev/null
+# Ensure that Python and pip are installed
+if ! command -v python3 &> /dev/null
 then
-    echo "pip not found, installing pip"
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python get-pip.py --user
+    echo "Python3 could not be found. Installing Python3..."
+    apt-get update
+    apt-get install -y python3 python3-pip
 fi
+
+# Create and activate a virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Upgrade pip
+pip install --upgrade pip
 
 # Install dependencies
 pip install -r requirements.txt
 
-python3.9 manage.py migrate 
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Deactivate virtual environment
+deactivate
