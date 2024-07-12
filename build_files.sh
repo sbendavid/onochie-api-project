6a285977468be06c4e92b6449cc49d9feed53a8f
sbendavid <samuelbendavid01@gmail.com>
2024-07-12 03:49:38 +0100

    Updated the build sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,23 @@
+echo "BUILD START"
+
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.12 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+
 # build_files.sh
 python3.12 -m pip install -r requirements.txt
 
 # make migrations
 python3.12 manage.py migrate 
 python3.12 manage.py collectstatic
+
+
+# collect static files using the Python interpreter from venv
+python manage.py collectstatic --noinput
+
+echo "BUILD END"
+
+# [optional] Start the application here 
+# python manage.py runserver
